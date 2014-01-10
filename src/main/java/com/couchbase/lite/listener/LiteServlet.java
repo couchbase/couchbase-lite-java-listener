4c452fadfd5d290b8acd6da37df226f8666dc668
Traun Leyden <traun@couchbase.com>
2014-01-10 14:58:03 -0800

    Remove debug message

--- src/main/java/com/couchbase/lite/listener/LiteServlet.java
+++ src/main/java/com/couchbase/lite/listener/LiteServlet.java
@@ -69,7 +69,6 @@ public class LiteServlet extends HttpServlet {
 
         final ServletOutputStream os = response.getOutputStream();
         response.setBufferSize(128);
-        Log.v(Database.TAG, String.format("Buffer size is %d", response.getBufferSize()));
 
         final CountDownLatch doneSignal = new CountDownLatch(1);
 
